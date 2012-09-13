855cf181654ea94d49d02a8c104334d14f4aff3c
Alex Burka <durka42@gmail.com>
2012-09-13 13:04:20 -0400

    color segmentation working as well as color segmentation ever can

--- manip/src/segment.cpp
+++ manip/src/segment.cpp
@@ -7,9 +7,11 @@
 #include <opencv2/nonfree/nonfree.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <image_view_plus/Click.h>
+#include <geometry_msgs/PoseArray.h>
 
 namespace enc = sensor_msgs::image_encodings;
 namespace it = image_transport;
+namespace geom = geometry_msgs;
 
 class Segmenter
 {
@@ -19,6 +21,7 @@ class Segmenter
             image_out_ = it_.advertise("image_out", 1);
             image_in_ = it_.subscribe("image_in", 1, &Segmenter::kick, this);
             click_sub_ = nh_.subscribe("click", 10, &Segmenter::click, this);
+            obj_pub_ = nh_.advertise<geom::PoseArray>("objects", 100);
             started_ = false;
             learned_ = 0;
         }
@@ -39,10 +42,83 @@ class Segmenter
             
             /* process image */
             cv::Mat output;
+            geom::PoseArray obj_msg;
+            geom::Pose pose;
 
             if (started_)
             {
+                const double THRESH = 40;
                 output = cv_ptr->image.clone();
+                cv::Mat r, g, b,
+                        rl, gl, bl,
+                        rh, gh, bh,
+                        mask = cv::Mat::zeros(last_image_.rows, last_image_.cols, CV_8UC1);
+                std::vector<cv::Mat> channels(3);
+                channels[0] = b; channels[1] = g; channels[2] = r;
+                cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(8, 8), cv::Point(4, 4));
+                output = cv::Mat::zeros(mask.rows, mask.cols, CV_8UC3);
+                for (int i = 0; i < 3; ++i)
+                {
+                    // mask out color
+                    cv::split(cv_ptr->image, channels);
+                    r = channels[2]; g = channels[1]; b = channels[0];
+                    cv::threshold(r, rl, color_[i][2] - THRESH, 1, CV_THRESH_BINARY);
+                    cv::threshold(g, gl, color_[i][1] - THRESH, 1, CV_THRESH_BINARY);
+                    cv::threshold(b, bl, color_[i][0] - THRESH, 1, CV_THRESH_BINARY);
+                    cv::threshold(r, rh, color_[i][2] + THRESH, 1, CV_THRESH_BINARY_INV);
+                    cv::threshold(g, gh, color_[i][1] + THRESH, 1, CV_THRESH_BINARY_INV);
+                    cv::threshold(b, bh, color_[i][0] + THRESH, 1, CV_THRESH_BINARY_INV);
+
+                    mask = rl.mul(gl).mul(bl).mul(rh).mul(gh).mul(bh) * 255;
+                    cv::morphologyEx(mask, mask, cv::MORPH_OPEN, element);
+                    cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, element);
+
+                    // find contours
+                    std::vector<std::vector<cv::Point> > contours;
+                    std::vector<cv::Vec4i> hierarchy;
+                    cv::findContours(mask, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+                    std::vector<cv::RotatedRect> ellipses(contours.size());
+                    int maxj = -1, max = 0;
+                    for (int j = 0; j < contours.size(); ++j)
+                    {
+                        if (contours[j].size() > 5)
+                        {
+                            ellipses[j] = cv::fitEllipse(cv::Mat(contours[j]));
+                            if (ellipses[j].size.width > max)
+                            {
+                                maxj = j;
+                                max = ellipses[j].size.width;
+                            }
+                            if (ellipses[j].size.height > max)
+                            {
+                                maxj = j;
+                                max = ellipses[j].size.height;
+                            }
+                        }
+                    }
+                    cv::drawContours(output, contours, maxj, color_[i], CV_FILLED);
+                    if (maxj != -1)
+                    {
+                        pose.position.x = ellipses[maxj].center.x;
+                        pose.position.y = ellipses[maxj].center.y;
+                        pose.position.z = 0;
+                        pose.orientation.x = 0;
+                        pose.orientation.y = 0;
+                        pose.orientation.z = sin(ellipses[maxj].angle/2);
+                        pose.orientation.w = cos(ellipses[maxj].angle/2);
+                    }
+                    else
+                    {
+                        pose.position.x = 0;
+                        pose.position.y = 0;
+                        pose.position.z = 0;
+                        pose.orientation.x = 0;
+                        pose.orientation.y = 0;
+                        pose.orientation.z = 0;
+                        pose.orientation.w = 1;
+                    }
+                    obj_msg.poses.push_back(pose);
+                }
             }
 
             /* convert back to ROS */
@@ -56,8 +132,9 @@ class Segmenter
                 out_msg.header   = msg->header;
                 out_msg.encoding = enc::BGR8;
                 out_msg.image    = output;
-
                 image_out_.publish(out_msg.toImageMsg());
+
+                obj_pub_.publish(obj_msg);
             }
 
             last_image_ = cv_ptr->image.clone();
@@ -65,16 +142,12 @@ class Segmenter
 
         void click(const image_view_plus::ClickConstPtr& msg)
         {
-            // sample from a 21x21 rectangle around the point
+            // sample from a rectangle around the point
             ROS_INFO("click received at (%d, %d)", msg->x, msg->y);
             if (!started_)
             {
-                ROS_INFO("rect is (%d, %d) - (%d, %d)", msg->x - 10, msg->y - 10, msg->x + 10, msg->y + 10);
-                //cv::Mat mask(last_image_.rows, last_image_.cols, CV_8UC3, 0);
-                //cv::rectangle(mask, cv::Point(msg->x - 10, msg->y - 10), cv::Point(msg->x + 10, msg->y + 10), CV_RGB(1, 1, 1), CV_FILLED);
-                cv::Mat rect(last_image_, cv::Rect(msg->x - 10, msg->y - 10, 21, 21));
-                //color_[learned_] = cv::mean(last_image_, mask);
-                color_[learned_] = cv::mean(rect);
+                cv::Mat rect(last_image_, cv::Rect(msg->x - 20, msg->y - 20, 41, 41));
+                color_[learned_] = cv::sum(rect)/(41.0*41.0);
                 ++learned_;
 
                 if (learned_ == 3)
@@ -96,6 +169,7 @@ class Segmenter
         it::Publisher image_out_;
         it::Subscriber image_in_;
         ros::Subscriber click_sub_;
+        ros::Publisher obj_pub_;
         cv::Mat last_image_;
         bool started_;
         int learned_;
