11a6989a0dc6ebe9b8cd18e1fdfdc47a22372a07
Alex Burka <durka42@gmail.com>
2012-09-13 13:04:39 -0400

    add learner node

---
+++ manip/src/learn.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseArray.h>
+
+namespace geom = geometry_msgs;
+
+class Learner
+{
+    public:
+        Learner()
+        {
+            obj_sub_ = nh_.subscribe("objects", 100, &Learner::kick, this);
+        }
+
+        void kick(const geom::PoseArrayConstPtr& msg)
+        {
+        }
+
+    private:
+        ros::NodeHandle nh_;
+        ros::Subscriber obj_sub_;
+};
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "manip_learn");
+
+    Learner student;
+    ros::spin();
+
+    return 0;
+}
+
